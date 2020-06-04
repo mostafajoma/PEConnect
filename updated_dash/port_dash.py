db55f9c508e721d53c93e2dc64f3297309ad3412
devinjnigro <djn25@georgetown.edu>
2020-06-04 03:54:24 -0400

    Update port_dash.py

--- updated_dash/port_dash.py
+++ updated_dash/port_dash.py
@@ -23,10 +23,6 @@ from web3.auto.gethdev import w3
 #w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 
 # For Kovan live testnet
-w3 = Web3(Web3.HTTPProvider("https://kovan.infura.io/v3/c939afba949d4aa2903246e8029e4d49"))
-
-
-
 w3 = Web3(Web3.HTTPProvider("https://kovan.infura.io/v3/{INFURA_PROJECT_ID}"))
 
 #w3.middleware_onion.inject(geth_poa_middleware, layer=0)
