18036591a9d4e8b55e41f3a94cffff225995f192
devinjnigro <djn25@georgetown.edu>
2020-06-04 03:53:00 -0400

    Update port_dash.py

--- updated_dash/port_dash.py
+++ updated_dash/port_dash.py
@@ -21,14 +21,14 @@ from web3.auto.gethdev import w3
 # 1 of the following networks must be selected
 # For local network
 #w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
-<<<<<<< HEAD
+
 # For Kovan live testnet
 w3 = Web3(Web3.HTTPProvider("https://kovan.infura.io/v3/c939afba949d4aa2903246e8029e4d49"))
 
 
-=======
+
 w3 = Web3(Web3.HTTPProvider("https://kovan.infura.io/v3/{INFURA_PROJECT_ID}"))
->>>>>>> 3199f5bd1c581592952b29da6f984cec1eb4abcd
+
 #w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 #w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
 
